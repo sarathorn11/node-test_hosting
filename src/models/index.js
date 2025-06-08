2d2425a91bf014061dbfda2e0c1a64b1bce9b74f
sarathorn11 <raths8546@gmail.com>
2025-06-08 19:12:51 +0700

    add

--- src/models/index.js
+++ src/models/index.js
@@ -1,17 +1,17 @@
-import { readdirSync } from 'fs'
-import path from 'path'
-import { Sequelize, DataTypes } from 'sequelize'
-import config from '../../config/config.json' assert { type: 'json' }
-import { fileURLToPath } from 'url'
-import associate from './associations.js'
+import { readdirSync } from 'fs';
+import path from 'path';
+import { Sequelize, DataTypes } from 'sequelize';
+import config from '../../config/config.json' assert { type: 'json' };
+import { fileURLToPath } from 'url';
+import associate from './associations.js';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const basename = path.basename(__filename)
-const db = {}
-const env = process.env.NODE_ENV || 'development'
-const dbConfig = config[env]
+const basename = path.basename(__filename);
+const db = {};
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env];
 
 const sequelize = new Sequelize(
   dbConfig.database,
@@ -23,30 +23,27 @@ const sequelize = new Sequelize(
       ...dbConfig.model,
     },
   }
-)
+);
 
 const importModels = async () => {
   const files = readdirSync(__dirname).filter(
-    (file) => file !== basename && file.slice(-3) === '.js'
-  )
-  try {
-    await Promise.all(
-      files.map(async (file) => {
-        if (file === 'associations.js') return
-        const modelPath = path.join(__dirname, file)
-        const { default: model } = await import(`file://${modelPath}`)
-        const sequelizeModel = model(sequelize, DataTypes)
-        db[sequelizeModel.name] = sequelizeModel
-      })
-    )
-  } catch (error) {
-    console.error(error)
-  }
-  associate(db)
+    (file) => file !== basename && file.endsWith('.js')
+  );
 
-  db.sequelize = sequelize
-}
+  await Promise.all(
+    files.map(async (file) => {
+      if (file === 'associations.js') return;
+      const modelPath = path.join(__dirname, file);
+      const { default: model } = await import(`file://${modelPath}`);
+      const sequelizeModel = model(sequelize, DataTypes);
+      db[sequelizeModel.name] = sequelizeModel;
+    })
+  );
 
-await importModels()
+  associate(db);
+  db.sequelize = sequelize;
+};
 
-export default db
+await importModels();
+
+export default db;
