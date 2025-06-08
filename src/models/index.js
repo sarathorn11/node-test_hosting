69700e5c4ef4b2c3d348f0faf26484426c1431f5
sarathorn11 <raths8546@gmail.com>
2025-06-08 19:15:52 +0700

    add

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,9 @@
-import { readdirSync } from 'fs';
-import path from 'path';
-import { Sequelize, DataTypes } from 'sequelize';
-import config from '../../config/config.json' assert { type: 'json' };
-import { fileURLToPath } from 'url';
-import associate from './associations.js';
+const fs = require('fs');
+const path = require('path');
+const { Sequelize, DataTypes } = require('sequelize');
+const config = require('../../config/config.json');
+const { fileURLToPath } = require('url');
+const associate = require('./associations.js');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,24 +26,31 @@ const sequelize = new Sequelize(
 );
 
 const importModels = async () => {
-  const files = readdirSync(__dirname).filter(
-    (file) => file !== basename && file.endsWith('.js')
+  const files = fs.readdirSync(__dirname).filter(
+    (file) => file !== basename && file.slice(-3) === '.js'
   );
 
-  await Promise.all(
-    files.map(async (file) => {
-      if (file === 'associations.js') return;
-      const modelPath = path.join(__dirname, file);
-      const { default: model } = await import(`file://${modelPath}`);
-      const sequelizeModel = model(sequelize, DataTypes);
-      db[sequelizeModel.name] = sequelizeModel;
-    })
-  );
+  try {
+    await Promise.all(
+      files.map(async (file) => {
+        if (file === 'associations.js') return;
+        const modelPath = path.join(__dirname, file);
+        const { default: model } = await import(`file://${modelPath}`);
+        const sequelizeModel = model(sequelize, DataTypes);
+        db[sequelizeModel.name] = sequelizeModel;
+      })
+    );
+  } catch (error) {
+    console.error('Error importing models:', error);
+  }
 
   associate(db);
   db.sequelize = sequelize;
 };
 
-await importModels();
+// Immediately-invoked async function
+(async () => {
+  await importModels();
+})();
 
-export default db;
+module.exports = db;
